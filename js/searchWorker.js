fa03c2cba516bdfb54bd5233b3154c40f4649552
TotallyTung <117299275+TotallyTung@users.noreply.github.com>
2025-07-09 18:57:25 +0700

    more accurate attribute search

--- js/searchWorker.js
+++ js/searchWorker.js
@@ -5,7 +5,7 @@ onmessage = (e) => {
         const keywords = query.split(/(?=\s#)/g).filter(w => w.trimLeft()[0] !== "#")[0]
         const attributeMatch = query.match(/(?<=#)(.*?)(?=\s#|$)/g);
         const cardMatch = (keywords ? keywords.split(" ").some(k => matchStr.includes(k)) : false) ||
-            (attributeMatch && attributeMatch.map(s=>s.split(/ (.+)/)).some(attribute_query => {
+            (attributeMatch && attributeMatch.map(s=>s.split(/ (.+)/)).every(attribute_query => {
                 const [attr, val] = attribute_query;
                 return (card.location && ((attr === "location" && card.location.value == val) || (attr === "floor" && card.location.floor.includes(Number(val))))) ||
                         (card[attr] && (typeof card[attr] !== "object") && String(card[attr]).toLowerCase().includes(val)) ||
